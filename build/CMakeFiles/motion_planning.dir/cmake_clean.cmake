b6ee7e468c51e5ced805ccf75c69466eb2b04342
Kyle Pedersen <kylepedersen@berkeley.edu>
2023-06-28 14:48:38 -0700

    setup cmake

---
+++ build/CMakeFiles/motion_planning.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/motion_planning.dir/main.cpp.o"
+  "motion_planning.pdb"
+  "motion_planning"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/motion_planning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World12!" << std::endl;
+    return 0;
+}
